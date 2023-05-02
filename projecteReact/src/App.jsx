589b0697de004f88d9e74b644adc6867a3bd217f
Iker <ikermbergua@gmail.com>
2023-05-02 17:42:40 +0200

    Arreglos Package Json y App.jsx

--- projecteReact/src/App.jsx
+++ projecteReact/src/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import LoginRegister  from './auth/LoginRegister';
 import Header from './layouts/Header';
 import Footer from "./layouts/Footer";
 import Equipo from "./Equipo";
 import Salon from "./Salon";
+import NotFound from "./NotFound";
 // import Contacta from "./Contacta";
 // import Map from "./Map;"
 // import Productes from "./Productes";
-import NotFound from "./NotFound";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Routes, Route } from "react-router-dom";
-import './App.css'
+import './App.css';
 
-function App() {
+export default function App() {
+
+  let [authToken, setAuthToken] = useState("");
+  let [usuari, setUsuari] = useState("");
+  let [idUsuari, setIdUsuari] = useState("");
+
   return (
     <>
-      {/* <UserContext.Provider value= { { usuari, setUsuari,authToken,setAuthToken, idUsuari, setIdUsuari }}>
+      <UserContext.Provider value= { { usuari, setUsuari, authToken, setAuthToken, idUsuari, setIdUsuari }}>
         {authToken ? (
-          <> */}
+          <> 
             <Header />
             <Routes>
               <Route path='*' element={<NotFound />} />
@@ -26,20 +31,17 @@ function App() {
               {/* <Route path='/productes' element={<Productes></Productes>}></Route> */}
               <Route path='/cart' element={<></>}></Route>
               <Route path='/citas' element={<></>}></Route>
-              <Route path='/salon' element={<Salon></Salon>}></Route>
+              <Route path='/salon' element={<Salon/>}></Route>
               {/* <Route path='/contacta' element={<Contacta></Contacta>}></Route> */}
-              <Route path='/equipo' element={<Equipo></Equipo>}></Route>
-              <Route path='/loginregister' element={<LoginRegister></LoginRegister>}></Route>
+              <Route path='/equipo' element={<Equipo/>}></Route>
               {/* <Route path="/about" element={<About />} /> */}
             </Routes>
             <Footer />
-          {/* </>
+           </>
         ) : (
           <LoginRegister />
         )}
-      </UserContext.Provider> */}
+      </UserContext.Provider>
     </>
   )
 }
-
-export default App
