7d9c315b23a432736eb11f5748e9999a47c3726c
Iker <ikermbergua@gmail.com>
2023-05-02 19:46:54 +0200

    Arreglos package.json y añadido login

--- projecteReact/src/auth/Login.jsx
+++ projecteReact/src/auth/Login.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import './loginregister.css'
+import './loginregister.css';
+import { useForm } from "react-hook-form";
+
 export default function Login() {
+    const { doLogin, error, setError } = useLogin();  
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => doLogin(data);
 
     return (
         <> 
@@ -13,17 +18,43 @@ export default function Login() {
                         </div>
                     </div>
                     <br/>
-                    <div class="login">
-                        <input type="text" placeholder="username" name="user" required="Aquest camp és obligatori" 
-                            minLength="6"
-                            maxLength="40"
-                            pattern="/^[a-zA-Z0-9._%+-]+@fp\.insjoaquimmir\.cat$/"/><br/>
-                        <input type="password" placeholder="password" name="password" required="Aquest camp és obligatori" 
-                            minLength="8"
-                            maxLength="20"
-                            pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/"/><br/>
-                        <input type="button" value="Login"/>
-                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div class="login">
+                            <input type="text" placeholder="Email" {...register("email", {
+                                required: "Aquest camp és obligatori",
+                                minLength: {
+                                    value: 6,
+                                    message: "El email tiene que tener mínimo 6 caracteres"
+                                },
+                                maxLength: {
+                                    value: 40,
+                                    message: "El email no puede contener mas de 40 caracteres"
+                                },
+                                pattern: {
+                                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                    message: "Inserte un email válido" 
+                            }})}/>
+                            {errors.email ? <div className="error">{errors.email.message}</div> : <></>}
+                            <br/>
+                            <input type="password" placeholder="Password" {...register("password", {
+                                required: "Aquest camp és obligatori",
+                                minLength: {
+                                    value: 8,
+                                    message: "La contraseña tiene que contener mínimo 8 caracteres"
+                                },
+                                maxLength: {
+                                    value: 20,
+                                    message: "La contraseña no puede contener más de 20 caracteres"
+                                },
+                                pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                                    message: "La contraseá tiene que contener al menos una minúscula, una mayuscula, i un número" 
+                            }})}/>
+                            {errors.password ? <div className="error">{errors.password.message}</div> : <></>}
+                            <br/>
+                            <input type="submit" value="Login"/>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
---
+++ projecteReact/src/hooks/useLogin.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../userContext";
+
+export default function doLogin() {
+    let [error, setError] = useState("");
+    let {authToken, setAuthToken, usuari, setUsuari, idUsuari, setIdUsuari} = useContext(UserContext);
+
+    const checkAuthToken = async () => {
+        if (sessionStorage.getItem('token')){
+            try {
+                const data = await fetch("http://127.0.0.1:8000/api/user", {
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json",
+                        "Authorization": 'Bearer '  + sessionStorage.getItem('token'),
+                    },
+                    method: "GET",
+                })
+                const resposta = await data.json();
+                if (resposta.success === true) {
+                    setAuthToken(sessionStorage.getItem('token'));
+                    setUsuari(resposta.user.email);
+                    setIdUsuari(resposta.user.id);
+                }else{
+                    console.log("La resposta no ha triomfat");
+                }            
+            } catch {
+                console.log("Error");
+            }
+        };
+    }
+    useEffect(()=>{
+        checkAuthToken()
+    }, []);
+
+    
+  const doLogin = async (data) => {
+    let email = data.email;
+    let password = data.password;
+    try {
+      const data = await fetch("http://127.0.0.1:8000/api/login", {
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({ email, password })
+      });
+      const resposta = await data.json();
+      if (resposta.success === true){
+        setAuthToken(resposta.authToken);
+        setUsuari(email);
+        if (! sessionStorage.getItem('token')){
+          sessionStorage.setItem('token', resposta.authToken);
+        }
+        checkAuthToken();
+      }else{ 
+        setError(resposta.message);
+      }
+    } catch {
+      console.log("Error");
+    }
+  };
+
+  return { doLogin, error, setError, checkAuthToken };
+}
