58899a2a7f253146de2833482bec025963cf0089
ikercc4.4 <ikercaste@gmail.com>
2023-05-10 16:54:58 +0200

    Cambios aplicados

--- projecteReact/src/Servicios.jsx
+++ projecteReact/src/Servicios.jsx
@@ -9,10 +9,12 @@ export default function Contacta() {
             <h1 className='titContacta' id="linkServicios">SERVICIOS</h1>
             <section class="servicios_div_1">
                 <h1 className='h1_Servicios'>MEN</h1>
+                <div className='div_Servicios'></div>
             </section>
 
             <section class="servicios_div_2">
                 <h1 className='h1_Servicios'>COLOR</h1>
+                <div className='div_Servicios'></div>
             </section>
         </div>
     </>
