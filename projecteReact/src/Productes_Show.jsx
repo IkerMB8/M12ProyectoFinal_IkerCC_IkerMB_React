17ab4059162756f21c8f10a0ac38616568462456
ikercc4 <ikercaste@gmail.com>
2023-05-16 18:42:45 +0200

    Comenzando a hacer Show

--- projecteReact/src/Productes_Show.jsx
+++ projecteReact/src/Productes_Show.jsx
@@ -5,15 +5,15 @@ export default function Productes() {
     return (
         <>
             <div className='show_box'>
-                <div className='left'>
+                <div className='left_show'>
                     <h1 className='h1_show'>ColorSilk Beautiful Color<sup>™</sup> Hair Color</h1>
                 </div>
 
-                <div className='mid'>
+                <div className='mid_show'>
                     <div class="card-artista card-artista0"></div>
                 </div>
 
-                <div className='right'>
+                <div className='right_show'>
 
                 </div>
             </div>
