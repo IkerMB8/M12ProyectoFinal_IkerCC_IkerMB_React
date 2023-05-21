3b1c8a5e1fe71135d1a6006a8cc94f3ee0264ccc
IkerMB8 <ikermbergua@gmail.com>
2023-05-21 17:11:16 +0200

    cambiados cancel y succes y añadida media query movil navbar

--- projecteReact/src/pages/Cancel.jsx
+++ projecteReact/src/pages/Cancel.jsx
@@ -1,9 +1,9 @@
 function Cancel() {
   return (
     <div>
-      <h1 className="mt-3">Purchase cancelled!</h1>
+      <h1 className="mt-3">Compra cancelada</h1>
       <a href="/" className="btn btn-danger mt-2">
-        Back to home
+        Volver al inicio
       </a>
     </div>
   );
--- projecteReact/src/pages/Success.jsx
+++ projecteReact/src/pages/Success.jsx
@@ -1,9 +1,9 @@
 function Success() {
   return (
     <div>
-      <h1 className="mt-3">Thanks for your purchase!</h1>
+      <h1 className="mt-3">¡Gracias por tu compra! Estamos emocionados de atenderte. Te notificaremos tan pronto como tu pedido esté listo para ser recogido. Esperamos verte pronto en nuestra tienda. ¡Gracias nuevamente por elegirnos!</h1>
       <a href="/" className="btn btn-success mt-2">
-        Back to home
+        Volver al inicio
       </a>
     </div>
   );
